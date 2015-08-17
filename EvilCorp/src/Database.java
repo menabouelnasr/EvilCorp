324f266f917a5b2591eb0b63c42ce52faa614c78
Mena Abouelnasr <menabouelnasr3@gmail.com>
2015-08-17 11:27:04 -0400

    WE DID IT...AGAIN!

--- EvilCorp/src/Database.java
+++ EvilCorp/src/Database.java
@@ -45,18 +45,31 @@ public class Database
 	{
 		
         String sql ="select count(*) as NumberOfAccounts from acc_Info where acc_Num = "+ check_Acc;
-
         ResultSet result= getFromDB(sql);
       
         result.next();
-		return result.getInt("NumberofAccounts");
+		return result.getInt("NUMBEROFACCOUNTS");
+		
 	}
-
 	public void setCheck_Acc(int check_Acc) 
 	{
 		this.check_Acc = check_Acc;
 	}
 
+	public int getCheck_Sav_Acc() throws SQLException 
+	{
+		
+        String sql ="select count(*) as NumberOfAccounts from acc_Info where Savings_Num = "+ check_Acc;
+        ResultSet result= getFromDB(sql);
+      
+        result.next();
+		return result.getInt("NUMBEROFACCOUNTS");
+	}
+	public void setSav_Check_Acc(int check_Acc) 
+	{
+		this.check_Acc = check_Acc;
+	}
+	
 	public void UpdateDB(String sql) throws SQLException 
 	{
 
@@ -67,15 +80,17 @@ public class Database
 
 	}
 
-	public void setAdd_Acc(int acc, String name, Double balance, String birthday) 
+	public void setAdd_Acc(int acc, String name, Double balance, String birthday, int Sacc, double Sbal) 
 	{
 		int account= acc;
 		String names= name;
 		Double bal = balance;
 		String bday= birthday;
+		int savings= Sacc;
+		double savings_Bal=Sbal;
 		
-		String sql ="INSERT INTO Acc_Info (ACC_NUM, ACC_NAME, BAL, BIRTHDAY) "
-        		+ "VALUES ("+ account + ",'"+ names + "', "+ bal + ",To_DATE('" + bday + "','mm/dd/yyyy'))";
+		String sql ="INSERT INTO Acc_Info (ACC_NUM, ACC_NAME, BAL, BIRTHDAY, Savings_Num, Savings_Bal) "
+        		+ "VALUES ("+ account + ",'"+ names + "', "+ bal + ",To_DATE('" + bday + "','mm/dd/yyyy')," + savings + "," + savings_Bal + ")";
 		try {
 			UpdateDB(sql);
 		} catch (SQLException e) {
@@ -86,16 +101,19 @@ public class Database
 	
 
 
-	public void setAdd_Trans(int acc, double trans_Amount, int trans_Type, String date, int status) 
+	public void setAdd_Trans(int acc, double trans_Amount, int trans_Type, String date, int status, String types) 
 	{
 		int account= acc;
 		double amount= trans_Amount;
 		int type = trans_Type;
 		String day = date;
 		int stat= status;
+		String sometype=types;
+	
 		
-		String sql2 = "INSERT INTO TRANSACTION (ACC_NUM, TRANS_AMOUNT, TRANS_TYPE, TRANS_DATE, Status, Trans_ID) " + 
-		"VALUES (" + account + "," + amount + "," + type + "," + "To_DATE('" + day + "','mm/dd/yyyy')," + stat + ",seq_transaction.nextVal )";
+		String sql2 = "INSERT INTO TRANSACTION (ACC_NUM, TRANS_AMOUNT, TRANS_TYPE, TRANS_DATE, Status, sometype, Trans_ID ) VALUES (" 
+		+ account + "," + amount + "," + type + "," + "To_DATE('" + day + "','mm/dd/yyyy')," + stat + ",'" + sometype +"',seq_transaction.nextVal)";
+		
 		try {
 			UpdateDB(sql2);
 		} catch (SQLException e) {
@@ -111,6 +129,7 @@ public class Database
 		ResultSet r = null;
 		int account = acc;
 		double bal= trans_bal;
+		double savings_trans;
 		
 		String sql = "Select bal from acc_Info where acc_Num = " + acc;
 		
@@ -118,15 +137,68 @@ public class Database
 			r=getFromDB(sql);
 			r.next();
 			bal=trans_bal + r.getDouble("bal");
+		
 			if( bal<0)
 			{
-				bal-= 35;
+				bal-= 45;
+				savings_trans= -bal;
+				sql = "Update acc_Info set bal = " + 0 + "where acc_Num = " + acc;
+				try 
+				{
+					UpdateDB(sql);
+				} 
+				catch (SQLException e) 
+				{
+					e.printStackTrace();
+				}
+				sql="Update acc_Info set savings_bal = savings_bal - "+ savings_trans + "where acc_Num = " + acc;
+				try 
+				{
+					UpdateDB(sql);
+				} 
+				catch (SQLException e) 
+				{
+					e.printStackTrace();
+				}
 			}
+			else
+			{
+				sql = "Update acc_Info set bal = " + bal + "where acc_Num = " + acc;
+				try 
+				{
+					UpdateDB(sql);
+				} 
+				catch (SQLException e) 
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void setSav_Update_Acc(int acc, double trans_bal)
+	{
+		ResultSet r = null;
+		int account = acc;
+		double bal= trans_bal;
+		
+		String sql = "Select savings_bal from acc_Info where acc_Num = " + acc;
+		
+		try {
+			r=getFromDB(sql);
+			r.next();
+			bal=trans_bal + r.getDouble("savings_bal");
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		sql = "Update acc_Info set bal = " + bal + "where acc_Num = " + acc;
+		sql = "Update acc_Info set savings_bal = " + bal + "where acc_Num = " + acc;
+		
 		try {
 			UpdateDB(sql);
 		} catch (SQLException e) {
@@ -151,6 +223,98 @@ public class Database
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+	
+	public void transferAccounts(int transfer_F, int transfer_T,  double T_amount)
+	{
+		int t_T=transfer_T;
+		int t_F= transfer_F;
+		double transA = T_amount;
+		double Sbal=0, Cbal=0;
+		if(t_F>t_T)
+		{
+			//transfer to checking = t_T
+			//saving = t_F
+			ResultSet r = null;
+			ResultSet r2= null;
+			
+			String sql = "Select savings_bal from acc_Info where acc_Num = " + t_T;
+			String sql2 = "Select bal from acc_Info where acc_Num = " + t_T;
+			
+			try 
+			{
+				r=getFromDB(sql);
+				r2=getFromDB(sql2);
+				r.next();
+				r2.next();
+				Sbal= r.getDouble("savings_bal") - transA;
+				Cbal = r2.getDouble("bal") + transA;
+				
+			} catch (SQLException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			sql2 = "Update acc_Info set bal = " + Cbal + "where acc_Num = " + t_T;
+			try 
+			{
+				UpdateDB(sql2);
+			} catch (SQLException e) 
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			sql = "Update acc_Info set savings_bal = " + Sbal + "where acc_Num = " + t_T;
+			try {
+				UpdateDB(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		else
+		{
+			//transfer to savings = t_T
+			//checking = t_F
+			ResultSet r = null;
+			ResultSet r2= null;
+			
+			String sql = "Select savings_bal from acc_Info where acc_Num = " + t_F;
+			String sql2 = "Select bal from acc_Info where acc_Num = " + t_F;
+			
+			try {
+				r=getFromDB(sql);
+				r2=getFromDB(sql2);
+				r.next();
+				r2.next();
+				Sbal= r.getDouble("savings_bal") + transA;
+				Cbal = r2.getDouble("bal") - transA;
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			sql = "Update acc_Info set savings_bal = " + Sbal + "where acc_Num = " + t_F;
+			try {
+				UpdateDB(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			sql2 = "Update acc_Info set bal = " + Cbal + "where acc_Num = " + t_F;
+			try {
+				UpdateDB(sql2);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
 	}
 
 
--- EvilCorp/src/EvilCorpApp.java
+++ EvilCorp/src/EvilCorpApp.java
@@ -37,12 +37,12 @@ public class EvilCorpApp
 	public static void main(String[] args) throws IOException, SQLException 
 	{
 		Scanner keyboard= new Scanner(System.in);
-		int accNum = 0, i;
-		double bal = 0, transAmount, finAmount=0, trans_bal=0, finalBal=0;
+		int accNum = 0, i, transfer_F=0, transfer_T=0;
+		double bal = 0, transAmount, finAmount=0, trans_bal=0, finalBal=0, Sbal=0;
 		boolean promptNum, promptName, promptBal, promptAmount;
 		String bday, date;
-		String accName, choice="y", start="y", close="n", remove;
-		int trans;
+		String accName, choice="y", start="y", close="n", remove, accType = null;
+		int trans, savingsNum=0;
 
 		ArrayList<Double> amount = new ArrayList<Double>();
 
@@ -53,15 +53,25 @@ public class EvilCorpApp
 	while(start.equalsIgnoreCase("Y"))
 	{
 		//if statement to check for acc # in DB
+		System.out.println("What account would you like to perform a transaction for? (C/S)");
+		accType=keyboard.next();
 		
 		do{
 			System.out.println("Please enter your account #: "); 
 			accNum= keyboard.nextInt();
 			promptNum= Validator.getAccNum(accNum); //validates account number input
+			if(accType.equalsIgnoreCase("c"))
+			{
 			evil.setCheck_Acc(accNum);
+			}
+			else
+			{
+			evil.setSav_Check_Acc(accNum);
+			}
+			
 		}while(promptNum==false);
 		
-		if(evil.getCheck_Acc()>0)
+		if(evil.getCheck_Acc()>0 || evil.getCheck_Sav_Acc()>0)
 		{
 			System.out.println("Account is available");
 		}
@@ -73,10 +83,12 @@ public class EvilCorpApp
 			int x = 100 + r.nextInt(999);
 			
 			accNum= x;
-			System.out.println("Your new account number is: " + accNum);
+			savingsNum= accNum+1;
+			System.out.println("Your new checking account number is: " + accNum);
+			System.out.println("Your new savings account number is : "+ savingsNum);
 			
 			do{
-			System.out.println("Please enter the name of your account: ");
+			System.out.println("Please enter the name on your account: ");
 			accName=keyboard.next();
 			promptName= Validator.getAccName(accName); //validates name input
 			}while(promptNum==false);
@@ -84,33 +96,40 @@ public class EvilCorpApp
 			//check acc # with DB table, if it exists, complete transactions and update table
 			
 			do{
-			System.out.println("Please enter your account balance: "); 
+			System.out.println("Please enter your checking account balance: "); 
 			bal=keyboard.nextDouble();
 			promptBal= Validator.getBal(bal); //validates balance input
 			}while(promptBal==false);
 			
+			System.out.println("Please enter your savings account balance: "); 
+			Sbal=keyboard.nextDouble();
 			
 			System.out.println("Please enter your birthdate (MM/DD/YYYY): "); 
 			bday=keyboard.next();
-			//promptBal= Validator.getBal(bal); //validates balance input
 			
-			evil.setAdd_Acc(accNum, accName, bal, bday);
+			evil.setAdd_Acc(accNum, accName, bal, bday, savingsNum, Sbal);
 		}
 		
 		
 		outerloop:
 		while(choice.equalsIgnoreCase("Y"))
 		{
-			//do{
-				//change trans types to 1 - Deposit, 2 - Check, 3 - Withdrawal, 4 - Debit Card
-			System.out.println("Enter a transaction type related to this account(1 - Deposit, 2 - Check, 3 - Withdrawal, 4 - Debit Card) or -1 to finish");
+			System.out.println("Enter a transaction type related to this account(1 - Deposit, 2 - Check, 3 - Withdrawal, 4 - Transfer) or -1 to finish");
 			trans=keyboard.nextInt();
+			
 			if(trans==-1)
 			{
 				break outerloop;
 			}
-			//promptTrans= Validator.getTrans(trans); //validates transaction type input
-			//}while(promptTrans==false);
+			
+			if(trans==4)
+			{
+				System.out.print("Transfer from: ");
+				transfer_F=keyboard.nextInt();
+				
+				System.out.print("Transfer to: ");
+				transfer_T=keyboard.nextInt();		
+			}
 			
 			do{
 			System.out.println("Enter the transaction amount:");
@@ -125,8 +144,20 @@ public class EvilCorpApp
 			
 			System.out.println("Enter the transaction date: (MM/DD/YYYY)");
 			date=keyboard.next();
-			evil.setAdd_Trans(accNum, finAmount, trans, date, 0);
 			
+			if(accType.equalsIgnoreCase("c"))
+			{
+				evil.setAdd_Trans(accNum, finAmount, trans, date, 1, accType);
+			}
+			else
+			{
+				evil.setAdd_Trans(accNum, finAmount, trans, date, 1, accType);
+			}
+			
+			if(trans==4)
+			{
+				evil.transferAccounts(transfer_F, transfer_T, transAmount);
+			}
 			
 			System.out.println("Would you like to enter another transaction? (Y/N) ");
 			choice= keyboard.next();
@@ -136,19 +167,45 @@ public class EvilCorpApp
 			{
 				 trans_bal +=amount.get(i);
 			}
-			evil.setUpdate_Acc(accNum,trans_bal);
 			
-			
-			String sql = "Select bal from acc_Info where acc_Num = " + accNum;
-			ResultSet rs= null;
-			try {
-				rs= evil.getFromDB(sql);
-				rs.next();
-				finalBal= rs.getDouble("bal");
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			if(accType.equalsIgnoreCase("c"))
+			{
+				evil.setUpdate_Acc(accNum,trans_bal);
 			}
+			else
+			{
+				evil.setSav_Update_Acc((accNum-1),trans_bal);
+			}
+			
+			//gets the updated checking account balance
+			
+			if(accType.equalsIgnoreCase("c"))
+			{
+				String sql = "Select bal from acc_Info where acc_Num = " + accNum;
+				ResultSet rs= null;
+				try {
+					rs= evil.getFromDB(sql);
+					rs.next();
+					finalBal= rs.getDouble("bal");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			else
+			{
+				String sql = "Select savings_bal from acc_Info where Savings_Num = " + accNum;
+				ResultSet rs= null;
+				try {
+					rs= evil.getFromDB(sql);
+					rs.next();
+					finalBal= rs.getDouble("savings_bal");
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+				
 			if(finalBal==0.0)
 			{
 				System.out.println("The account balance is $0.00, would you like to remove your account? (Y/N)");
@@ -161,11 +218,10 @@ public class EvilCorpApp
 			}
 			System.out.println("Would you like to enter information for another account? (Y/N)");
 			start=keyboard.next();
+			choice="y";
 	}
 		System.out.println("Thank you for banking with Evil Corp :)");
 		Database.conn.close();
 	}
 	
 }
-	// cut code	
-		
